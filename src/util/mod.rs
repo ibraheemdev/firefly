a196c7d862003fbe9ea4112e31a22bd7cc43ad21
Ibraheem Ahmed <ibraheem@ibraheem.ca>
2022-06-12 11:35:49 -0400

    remove unneeded utils

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -81,55 +81,6 @@ impl<T> UnsafeDeref<T> for *mut T {
     }
 }
 
-// Pads and aligns a value to the length of two cache lines.
-#[cfg_attr(
-    any(
-        target_arch = "x86_64",
-        target_arch = "aarch64",
-        target_arch = "powerpc64",
-    ),
-    repr(align(256))
-)]
-#[cfg_attr(
-    any(
-        target_arch = "arm",
-        target_arch = "mips",
-        target_arch = "mips64",
-        target_arch = "riscv64",
-    ),
-    repr(align(64))
-)]
-#[cfg_attr(target_arch = "s390x", repr(align(512)))]
-#[cfg_attr(
-    not(any(
-        target_arch = "x86_64",
-        target_arch = "aarch64",
-        target_arch = "powerpc64",
-        target_arch = "arm",
-        target_arch = "mips",
-        target_arch = "mips64",
-        target_arch = "riscv64",
-        target_arch = "s390x",
-    )),
-    repr(align(128))
-)]
-#[derive(Default)]
-pub struct DoubleCachePadded<T>(pub T);
-
-impl<T> std::ops::Deref for DoubleCachePadded<T> {
-    type Target = T;
-
-    fn deref(&self) -> &T {
-        &self.0
-    }
-}
-
-impl<T> std::ops::DerefMut for DoubleCachePadded<T> {
-    fn deref_mut(&mut self) -> &mut T {
-        &mut self.0
-    }
-}
-
 pub trait FetchAddPtr<T> {
     fn fetch_add(&self, n: usize, ordering: Ordering) -> *mut T;
 }
