39df01c3d26ed2a331bebd5f1737dd08642cdee1
Ibraheem Ahmed <ibraheem@ibraheem.ca>
2022-10-16 19:43:54 -0400

    fix bounded spsc queue

--- src/channels/spsc/bounded.rs
+++ src/channels/spsc/bounded.rs
@@ -1,10 +1,11 @@
 use crate::raw::parking;
-use crate::raw::util::CachePadded;
+use crate::raw::util::{assert_valid_capacity, CachePadded};
 
 use std::cell::{Cell, UnsafeCell};
 use std::mem::MaybeUninit;
 use std::sync::atomic::{AtomicUsize, Ordering};
 
+// A basic ring buffer
 pub struct Queue<T> {
     head: CachePadded<AtomicUsize>,
     tail: CachePadded<AtomicUsize>,
@@ -15,24 +16,21 @@ unsafe impl<T: Send> Send for Queue<T> {}
 
 impl<T> Queue<T> {
     pub fn new(capacity: usize) -> Self {
-        let capacity = capacity.next_power_of_two();
+        assert_valid_capacity(capacity);
 
         Self {
             head: CachePadded(AtomicUsize::new(0)),
             tail: CachePadded(AtomicUsize::new(0)),
-            slots: (0..capacity)
+            // need one extra slot
+            slots: (0..capacity + 1)
                 .map(|_| UnsafeCell::new(MaybeUninit::uninit()))
                 .collect(),
         }
     }
 
-    pub fn is_empty(&self) -> bool {
-        self.head.load(Ordering::Relaxed) == self.tail.load(Ordering::Relaxed)
-    }
-
     #[inline]
-    fn next(&self, pos: usize) -> usize {
-        let pos = pos + 1;
+    fn next(&self, mut pos: usize) -> usize {
+        pos += 1;
         if pos == self.slots.len() {
             0
         } else {
@@ -41,6 +39,7 @@ impl<T> Queue<T> {
     }
 }
 
+// A local, cached copy of the queue's head/tail
 pub struct Handle {
     head: Cell<usize>,
     tail: Cell<usize>,
@@ -60,20 +59,28 @@ impl Handle {
 
         let next_tail = queue.next(tail);
 
+        // is the queue full according to our cached head?
         if next_tail == head {
+            // load the current head
             let head = queue.head.load(Ordering::Acquire);
+
+            // update our cache
             self.head.set(head);
+
+            // is the queue really full?
             if next_tail == head {
                 return Err(value);
             }
         }
 
+        // store the element
         queue
             .slots
             .get_unchecked(tail)
             .get()
             .write(MaybeUninit::new(value));
 
+        // update the tail
         queue.tail.store(next_tail, parking::RELEASE);
         self.tail.set(next_tail);
 
@@ -84,16 +91,24 @@ impl Handle {
         let head = self.head.get();
         let tail = self.tail.get();
 
+        // is the queue empty according to our cached tail?
         if head == tail {
+            // load the current tail
             let tail = queue.tail.load(Ordering::Acquire);
+
+            // update our cache
             self.tail.set(tail);
+
+            // is the queue really empty?
             if tail == head {
                 return None;
             }
         }
 
+        // read the value
         let value = queue.slots.get_unchecked(head).get().read().assume_init();
 
+        // update the head
         let head = queue.next(head);
         queue.head.store(head, parking::RELEASE);
         self.head.set(head);
